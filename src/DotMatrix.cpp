e1c995654f115f2ec840745e5bb6e4ed3f68402c
SinoXZH <158701073@qq.com>
2019-03-11 21:00:11 +0800

    3.11 commit 2

---
+++ src/DotMatrix.cpp
@@ -0,0 +1,41 @@
+#include "DotMatrix.h"
+#include <fstream>
+
+DotMatrix::DotMatrix()
+{
+
+}
+
+DotMatrix::~DotMatrix()
+{
+
+}
+
+bool DotMatrix::CreateNewEmptyFile(unsigned int width, unsigned int height)
+{
+    if (width == 0 || height == 0) {
+        return false;
+    }
+
+    ofstream ofs;
+    ofs.open(filePath.c_str(), ios_base::out);
+    if (!ofs.is_open()) {
+        return false;
+    }
+
+    string content;
+
+    for (unsigned int h = 0; h < height; ++h) {
+        for (unsigned int w = 0; w < width; ++w) {
+            content += '.';
+        }
+        content += '\n';
+    }
+
+    ofs << content;
+
+    ofs.close();
+
+    return true;
+}
+
---
+++ src/DotMatrix.h
@@ -0,0 +1,22 @@
+#ifndef __DOT_MATRIX_H__
+#define __DOT_MATRIX_H__
+
+#include <string>
+using namespace std;
+
+class DotMatrix {
+public:
+    DotMatrix();
+    ~DotMatrix();
+
+public:
+    bool CreateNewEmptyFile(unsigned int width, unsigned int height);
+
+public:
+    string filePath;
+
+};
+
+
+#endif // __DOT_MATRIX_H__
+
